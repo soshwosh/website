766f6b90cc925ac0915f8b4859e9f2e1f9263d12
soshwosh <sasharavpersaud@gmail.com>
2024-10-05 18:18:48 -0400

    added functionality to social media icon buttons

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navBar = document.querySelector('.navbar');
-
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x')
     navBar.classList.toggle('active');
 }
+
+const githubButtons = document.querySelectorAll('.github');
+githubButtons.forEach(button => {
+    button.addEventListener('click', function() {
+        window.location.href = "https://github.com/soshwosh";
+    });
+});
+
+const linkedinButtons = document.querySelectorAll('.linkedin');
+linkedinButtons.forEach(button => {
+    button.addEventListener('click', function() {
+        window.location.href = "https://www.linkedin.com/in/sasha-r-persaud/";
+    });
+});
+
+const instagramButtons = document.querySelectorAll('.instagram');
+instagramButtons.forEach(button => {
+    button.addEventListener('click', function() {
+        window.location.href = "https://www.instagram.com/sosh.goes.to.ghc/";
+    });
+});
